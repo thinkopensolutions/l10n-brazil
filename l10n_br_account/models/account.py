5e692442157ed22ac87a93e6d309d398bb7caa2d
Yogesh Kushwaha <yogeshkushwaha4@gmail.com>
2016-10-05 10:21:47 -0300

    Revert "fix after merge upstream"

--- l10n_br_account/models/account.py
+++ l10n_br_account/models/account.py
@@ -2,7 +2,6 @@
 # Copyright (C) 2009 - TODAY Renato Lima - Akretion                           #
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-import openerp
 from openerp import models, fields, api
 import openerp.addons.decimal_precision as dp
 from openerp import SUPERUSER_ID
